da5fada4a8d563ecf6a931deaa5dfc543daf6ed8
tsheporamantso <tshepo.ramantso@outlook.com>
2026-08-13 12:38:50 +0200

    feat: add chatbot welcome message

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,9 +1,14 @@
 import styles from "./Chat.module.css";
 
+const WELCOME_MESSAGE = {
+  role: "assistant",
+  content: "Hello!, how can I assist you?",
+};
+
 export const Chat = ({ messages }) => {
   return (
     <div className={styles.Chat}>
-      {messages.map(({ role, content }, index) => {
+      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => {
         return (
           <div key={index} data-role={role} className={styles.Message}>
             {content}
