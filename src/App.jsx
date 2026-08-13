541c43aa6e533d92559adb2c44f32e8dc8bd3b3c
tsheporamantso <tshepo.ramantso@outlook.com>
2026-08-13 10:51:56 +0200

    feat:add placeholder messages and render chat component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
 import styles from "./App.module.css";
+import { Chat } from "./components/Chat/Chat";
+import { useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState(MESSAGES);
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
         <img src="/chat-bot.png" className={styles.Logo} />
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div className={styles.ChatContainer}></div>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
     </div>
   );
 }
+
+const MESSAGES = [
+  {
+    role: "user",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit error harum atque deleniti, accusantium autem blanditiis eos aliquam eveniet natus dolores cumque possimus quasi aut voluptas! Quam similique ad odio?",
+  },
+  {
+    role: "assistant",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit error harum atque deleniti, accusantium autem blanditiis eos aliquam eveniet natus dolores cumque possimus quasi aut voluptas! Quam similique ad odio?",
+  },
+  {
+    role: "user",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit error harum atque deleniti, accusantium autem blanditiis eos aliquam eveniet natus dolores cumque possimus quasi aut voluptas! Quam similique ad odio?",
+  },
+  {
+    role: "assistant",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit error harum atque deleniti, accusantium autem blanditiis eos aliquam eveniet natus dolores cumque possimus quasi aut voluptas! Quam similique ad odio?",
+  },
+  {
+    role: "user",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit error harum atque deleniti, accusantium autem blanditiis eos aliquam eveniet natus dolores cumque possimus quasi aut voluptas! Quam similique ad odio?",
+  },
+  {
+    role: "assistant",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit error harum atque deleniti, accusantium autem blanditiis eos aliquam eveniet natus dolores cumque possimus quasi aut voluptas! Quam similique ad odio?",
+  },
+];
 
 export default App;
