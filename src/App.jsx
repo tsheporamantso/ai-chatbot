f919957edbadb6f003cb3c051eb2af17405a15b5
tsheporamantso <tshepo.ramantso@outlook.com>
2026-08-13 15:06:30 +0200

    feat: connect gemini AI for chatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import styles from "./App.module.css";
 import { Chat } from "./components/Chat/Chat";
 import { useState } from "react";
 import Controls from "./components/Controls/Controls";
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+const chat = ai.chats.create({
+  model: "gemini-3.6-flash",
+});
 
 function App() {
   const [messages, setMessages] = useState([]);
 
-  const handleContentSend = (content) => {
-    setMessages((prevMessage) => [...prevMessage, { content, role: "user" }]);
+  const addMessage = (message) => {
+    setMessages((prevMessage) => [...prevMessage, message]);
+  };
+
+  const handleContentSend = async (content) => {
+    addMessage({ content, role: "user" });
+
+    try {
+      const response = await chat.sendMessage({ message: content });
+      addMessage({ content: response.text, role: "assistant" });
+    } catch (error) {
+      console.error(error);
+      addMessage({
+        content: "Sorry I couldn't process your request. Please try again!",
+        role: "system",
+      });
+    }
   };
   return (
     <div className={styles.App}>
