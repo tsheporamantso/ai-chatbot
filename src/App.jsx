e4a89562e9c26d89466171f9f2d482c3b5b39d41
tsheporamantso <tshepo.ramantso@outlook.com>
2026-08-16 10:47:02 +0200

    feat: enable dark mode with light-dark css property

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import styles from "./App.module.css";
 import { Chat } from "./components/Chat/Chat";
 import { useState } from "react";
 import Controls from "./components/Controls/Controls";
-// import { Assistant } from "./components/assistants/googleai";
-import { Assistant } from "./components/Assistants/openai";
+import { Assistant } from "./components/assistants/googleai";
+// import { Assistant } from "./components/Assistants/openai";
 import Loaders from "./components/Loader/Loaders";
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const responseText = await assistant.chat(content, messages);
+      const responseText = await assistant.chat(content);
       addMessage({ content: responseText, role: "assistant" });
     } catch (error) {
       console.error(error);
